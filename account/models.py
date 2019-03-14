505877b89897d86550005b4a2386daac19dbe1cf
Simon MWEPU <simonmwepu@gmail.com>
2019-03-14 16:47:23 +0200

    trying to fix bug model

--- account/models.py
+++ account/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.password_validation import password_changed
+from django.db.models.fields import CharField
+from unittest.util import _MAX_LENGTH
 
 class CreateAccount(models.Model):
-    pass
+    surname = models.CharField(_MAX_LENGTH = 100)
+    name = models.CharField(_MAX_LENGTH = 100)
