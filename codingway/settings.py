9f7c753c418c977d5bd8447bbb1efcc1e61f5604
Simon MWEPU <simonmwepu@gmail.com>
2019-03-14 10:37:07 +0200

    new Dirs for account templates

--- codingway/settings.py
+++ codingway/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'codingway.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['account/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
