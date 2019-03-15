8cb74998106d45bea0332c2049fb08652d70a849
Simon MWEPU <simonmwepu@gmail.com>
2019-03-15 08:02:09 +0200

    __str__() method added

--- account/models.py
+++ account/models.py
@@ -6,3 +6,6 @@ class CreateAccount(models.Model):
     email = models.CharField(max_length = 100)
     phone = models.CharField(max_length = 13)
     password = models.CharField(max_length = 100)
+
+    def __str__(self):
+        pass
