8415accfe5705989904c58b84bb0900b9e9d2d95
Simon MWEPU <simonmwepu@gmail.com>
2019-03-16 10:45:11 +0200

    rename class to UserCreateView

--- account/views.py
+++ account/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import CreateView #import CreateView
-from .models import CreateAccount
+from account.models import CreateAccount
 
-class AccountCreateView(CreateView):
+class UserCreateView(CreateView):
     
     fields = ('surname', 'name', 'email', 'phone', 'password')
 
-def v_account(request):
-    
-    return render(request, 'account.html')
+"""def v_account(request):
+
+    return render(request, 'account.html')"""
