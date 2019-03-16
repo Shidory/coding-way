f7063816dc45ce7841d7236d06f786d42faea1e2
Simon MWEPU <simonmwepu@gmail.com>
2019-03-16 10:13:41 +0200

    AccountCreateView class created

--- account/views.py
+++ account/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+
 from .models import CreateAccount
+
+class AccountCreateView(CreateView):
+    pass
 
 def v_account(request):
     return render(request, 'account.html')
