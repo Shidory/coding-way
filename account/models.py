1d45170f2250869b98bf3b729297baa4700614c1
Simon MWEPU <simonmwepu@gmail.com>
2019-03-16 10:44:07 +0200

    rename class to User

--- account/models.py
+++ account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class CreateAccount(models.Model):
+class User(models.Model):
 
     surname = models.CharField(max_length = 50)
     name = models.CharField(max_length = 50)
@@ -8,6 +8,6 @@ class CreateAccount(models.Model):
     phone = models.CharField(max_length = 13)
     password = models.CharField(max_length = 100)
 
-    def __str__(self):
+    """def __str__(self):
         
-        return self.surname, self.name, self.email, self.phone, self.password
+        return self.surname, self.name, self.email, self.phone, self.password"""
