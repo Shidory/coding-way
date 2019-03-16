c81c70ec47251329f154ffdc09b0c2dcfaed06a4
Simon MWEPU <simonmwepu@gmail.com>
2019-03-16 20:19:05 +0200

    try to load login.html

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from account import views
 
 urlpatterns = [
-    path('', views.v_account, name='v_account'),
+    #path('', views.v_account, name='v_account'),
+    path('', 'templates/login.html'),
 ]
