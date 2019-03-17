62ecdd2ad195454533b59658a5b2cd5083194b4a
Simon MWEPU <simonmwepu@gmail.com>
2019-03-17 21:53:17 +0200

    add path for home

--- codingway/urls.py
+++ codingway/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('account/', include('account.urls')),
+    path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
