2552fb98416a1bec7a1dcaff507b3d7388135184
Simon MWEPU <simonmwepu@gmail.com>
2019-03-19 09:04:51 +0200

    add contact path

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from account import views
 
 urlpatterns = [
-    path('', views.account_controller, name='account_controller'),
+    #path('', views.account_controller, name='account_controller'),
+    path('', views.contact),
 ]
