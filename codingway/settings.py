6320acef3e1580dc8d64723f494c85a51accc6a2
Simon MWEPU <simonmwepu@gmail.com>
2019-03-19 08:59:35 +0200

    add setting account

--- codingway/settings.py
+++ codingway/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',#account.............
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
