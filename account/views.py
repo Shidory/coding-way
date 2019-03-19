5c583c3cabe421e3aa09ed9a988747df1c4e1e2b
Simon MWEPU <simonmwepu@gmail.com>
2019-03-19 22:57:42 +0200

    try to print email and name

--- account/views.py
+++ account/views.py
@@ -23,5 +23,6 @@ def contact(request):
         if form.is_valid():
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
+            print(name,email)
     form = ContactForm()
     return render(request, 'account/form.html', {'form': form})
